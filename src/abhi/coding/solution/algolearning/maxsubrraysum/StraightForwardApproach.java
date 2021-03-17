e21af69805785a2c131a1b8a8c229d8af21666dc
AbhiAndroidManiac <abhiandymania@gmail.com>
2021-03-17 08:21:09 -0700

    algolearning commit 17th mar' 2020 finishing max sub array sum.

--- src/abhi/coding/solution/algolearning/maxsubrraysum/StraightForwardApproach.java
+++ src/abhi/coding/solution/algolearning/maxsubrraysum/StraightForwardApproach.java
@@ -1,11 +1,38 @@
 package abhi.coding.solution.algolearning.maxsubrraysum;
 
 public class StraightForwardApproach {
+    //time complexity is o(n)^3
     int best =0;
     public static void main(String[] args) {
+        StraightForwardApproach straightForwardApproach=new StraightForwardApproach();
         int[] a={-1,2,4,-3,5,2,-5,2};
+        straightForwardApproach.maximumSum(a);
+        straightForwardApproach.printSubarray(a);
+        System.out.println(straightForwardApproach.best);
     }
     public void maximumSum(int[] a){
-        
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = i; j <a.length ; j++) {
+                int sum=0;
+                for (int k = i; k <=j ; k++) {
+                    sum+=a[k];
+                }
+                best=Math.max(sum,best);
+            }
+        }
+    }
+    //with every loop the start shits right
+    private void printSubarray(int[] a){
+        for (int i = 0; i <a.length ; i++) {
+            System.out.print("[");
+            for (int j = i; j <a.length ; j++) {
+                if (j==a.length-1)
+                System.out.print(a[j]);
+                else
+                System.out.print(a[j]+", ");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
     }
 }
