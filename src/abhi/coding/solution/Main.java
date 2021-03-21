19812fda275e013be1ef251bcf95aff7df99aba8
AbhiAndroidManiac <abhiandymania@gmail.com>
2021-03-21 07:14:50 -0700

    algolearning commit 21st mar' 2020

--- src/abhi/coding/solution/Main.java
+++ src/abhi/coding/solution/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
+        String a="hatti";
+        String b=a+a;
+        System.out.println(b);
+        char[] c=b.toCharArray();
+        c[5]='v';
+        StringBuilder cb=new StringBuilder();
+        for (int i = 0; i <c.length ; i++) {
+            cb.append(c[i]);
+        }
+        b= cb.toString();
+        System.out.println(b);
+        String d=b.substring(3,b.length());
+        System.out.println(d);
     }
 }
