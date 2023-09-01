d04a534bed173087f857f680dabacc009e0a9ccc
Abhishek <abhishek.raj@netcommlabs.in>
2023-09-01 15:13:23 +0530

    E:BFromA august 2023 hackanthon solution

--- src/abhi/coding/solution/hackerearth/augustcircuit2023/BFromA.java
+++ src/abhi/coding/solution/hackerearth/augustcircuit2023/BFromA.java
@@ -1,69 +1,45 @@
 package abhi.coding.solution.hackerearth.augustcircuit2023;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class BFromA {
-    List listA = new ArrayList();
-    List listB = new ArrayList();
-    public static void main(String[] args) {
-        String[] a={"h","g","e","b"};
-        String[] b={"e","b","h"};
-        BFromA bFromA = new BFromA();
-        bFromA.listA=Arrays.asList(a);
-        bFromA.listB=Arrays.asList(b);
-
-        HashMap<String,Integer> validatorMap = new HashMap<>();
-        for (int i = 0; i <bFromA.listB.size() ; i++) {
-            validatorMap.put(bFromA.listB.get(i).toString(),0);
-        }
-        validateExistence(validatorMap,bFromA.listA);
-        for (String name: validatorMap.keySet()) {
-            System.out.print(validatorMap.get(name)+" ");
+    public static void main(String args[] ) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(reader.readLine());
+        while (testCases-- > 0) {
+            String line = reader.readLine();
+            int N = Integer.parseInt(line.charAt(0) + "");
+            int D = Integer.parseInt(line.charAt(2) + "");
+            String A = reader.readLine();
+            String B = reader.readLine();
+            List<String> listA=new LinkedList(Arrays.asList(A.split("")));
+            List<String> listB=new LinkedList(Arrays.asList(B.split("")));
+            LinkedHashMap<String,Integer> countLinkedHashMap = new LinkedHashMap<>();
+            LinkedHashMap<String,Integer> totCountHashMap = new LinkedHashMap<>();
+            for (int i = 0; i <listB.size() ; i++) {
+                if (countLinkedHashMap.containsKey(listB.get(i))){
+                    countLinkedHashMap.put(listB.get(i),countLinkedHashMap.get(listB.get(i))+1);
+                } else {
+                    countLinkedHashMap.put(listB.get(i),1);
+                }
+                totCountHashMap.put(listB.get(i),0);
+            }
+            System.out.println(validateExistence(countLinkedHashMap,listA,totCountHashMap));
         }
     }
-
-    private static int validateExistence(HashMap<String,Integer> validatorMap,List a){
-        int count = 0;
-        for (int i = 0; i <a.size() ; i++) {
-            //check if the element of the bigger array is present
-            if (validatorMap.containsKey(a.get(i))){
-                //check if the character is already read and added in the hashmap
-                if (validatorMap.get(a.get(i))==0){
-                    //if not add the index in the value against the character key
-                    validatorMap.put(String.valueOf(a.get(i)),i);
-                }
-            }
+    private static int validateExistence(LinkedHashMap<String,Integer> countMap,List<String> a,LinkedHashMap<String,Integer> totCountMap){
+        for (String key: totCountMap.keySet()){
+            totCountMap.put(key,(Collections.frequency(a,key)/countMap.get(key)));
         }
-        for (String name: validatorMap.keySet()) {
-            System.out.print(validatorMap.get(name)+" ");
-            if (validatorMap.get(name)==0){
-                count=0;
-            } else {
-                count=1;
-            }
-        }
-        if (count==1){
-
-        }
-        return count;
+        int min = Collections.min(totCountMap.values());
+        return min;
     }
 }
 
 
 
 
-
-
-  /* int[] a= {2, 3 ,1};
-        //core for creating all possible subsets of the array
-        for (int i = 0; i < a.length; i++)
-            // For loop for end index
-            for (int j = i; j < a.length; j++) {
-                // For loop to print subarray elements
-                ArrayList<Integer> b  = new ArrayList<>();
-                for (int k = i; k <=j; k++) {
-                    b.add(a[k]);
-                    System.out.print(a[k] + " ");
-                }
-                System.out.println();
-            }*/
