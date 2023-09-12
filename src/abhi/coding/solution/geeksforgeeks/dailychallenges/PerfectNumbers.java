b5fa49f8835c071ba58f8811acd2aecc5d4c0a31
Abhishek <abhishek.raj@netcommlabs.in>
2023-09-12 17:02:24 +0530

    E:added info about the problem of the day geeks for geeks

--- src/abhi/coding/solution/geeksforgeeks/dailychallenges/PerfectNumbers.java
+++ src/abhi/coding/solution/geeksforgeeks/dailychallenges/PerfectNumbers.java
@@ -2,6 +2,33 @@ package abhi.coding.solution.geeksforgeeks.dailychallenges;
 
 import java.util.LinkedList;
 
+
+/*Given a number N, check if a number is perfect or not.
+        A number is said to be perfect if sum of all its factors
+        excluding the number itself is equal to the number.
+        Return 1 if the number is Perfect otherwise return 0.
+
+
+        Input:
+N = 6
+Output:
+1
+Explanation:
+Factors of 6 are 1, 2, 3 and 6.
+Excluding 6 their sum is 6 which
+is equal to N itself. So, it's a
+Perfect Number.
+
+
+Input:
+N = 10
+Output:
+0
+Explanation:
+Factors of 10 are 1, 2, 5 and 10.
+Excluding 10 their sum is 8 which
+is not equal to N itself. So, it's
+not a Perfect Number.*/
 public class PerfectNumbers {
     public static void main(String[] args) {
         long num = 6L;
